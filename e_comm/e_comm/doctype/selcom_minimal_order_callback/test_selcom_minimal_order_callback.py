cde1b25e50428b99a968db487b8277634aec764f
Your Name <your.email@example.com>
2025-08-25 15:26:51 +0200

    Initial commit for toka sokoni mobile app

---
+++ e_comm/e_comm/doctype/selcom_minimal_order_callback/test_selcom_minimal_order_callback.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Arnold I Simony and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestSelcomMinimalOrderCallback(FrappeTestCase):
+	pass
